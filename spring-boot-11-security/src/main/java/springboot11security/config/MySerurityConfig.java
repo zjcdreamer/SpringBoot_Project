d08b8d7455ce6b23b5c8e8dcb095d096ca790136
zhangjincheng <382462237@qq.com>
2020-04-08 20:00:14 +0800

    SpringBoot与SpringSecurity整合

---
+++ spring-boot-11-security/src/main/java/springboot11security/config/MySerurityConfig.java
@@ -0,0 +1,14 @@
+package springboot11security.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class MySerurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //super.configure(http);
+
+    }
+}
---
+++ spring-boot-11-security/src/test/java/springboot11security/SpringBoot11SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot11security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot11SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
