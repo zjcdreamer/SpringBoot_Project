a31040081ec6512e1be34ef38d53b8f482f88cd1
zhangjincheng <382462237@qq.com>
2020-04-09 17:41:55 +0800

    SpringBoot与SpringSecurity整合

--- spring-boot-11-security/src/main/java/springboot11security/config/MySerurityConfig.java
+++ spring-boot-11-security/src/main/java/springboot11security/config/MySerurityConfig.java
@@ -1,14 +1,50 @@
 package springboot11security.config;
 
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 public class MySerurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //super.configure(http);
+        //定制请求的授权规则
+        http.authorizeRequests().antMatchers("/").permitAll()
+                .antMatchers("/level1/**").hasRole("VIP1")
+                .antMatchers("/level2/**").hasRole("VIP2")
+                .antMatchers("/level3/**").hasRole("VIP3");
 
+        //开启自动配置的登陆功能
+        /*
+            1、来到/login登录页
+            2、如果失败了会重定向到 /login?error
+         */
+        http.formLogin().loginPage("/userlogin");
+
+        /*
+            开启自动配置的注销功能
+            1、访问 /logout 表示用户注销，清空session
+            2、成功退出后，来到/login?logout这个页面
+
+         */
+        http.logout().logoutSuccessUrl("/");//这样就配置了注销成功之后跳转到那个页面
+
+        //开启记住我功能
+        http.rememberMe();
+    }
+
+    //定义认证规则
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //super.configure(auth);
+        auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .withUser("zhangsan").password(new BCryptPasswordEncoder().encode("123456")).roles("VIP1","VIP2")
+                .and()
+                .withUser("lisi").password(new BCryptPasswordEncoder().encode("123456")).roles("VIP2","VIP3")
+                .and()
+                .withUser("wangwu").password(new BCryptPasswordEncoder().encode("123456")).roles("VIP1","VIP3");
     }
 }
