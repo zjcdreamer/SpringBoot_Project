d350c81f454e897a0f35450ddd711b3eb6206c1f
zhangjincheng <382462237@qq.com>
2020-04-11 11:14:32 +0800

    监督

---
+++ spring-boot-12-actuator/src/main/java/springboot12actuator/SpringBoot12ActuatorApplication.java
@@ -0,0 +1,13 @@
+package springboot12actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot12ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot12ActuatorApplication.class, args);
+    }
+
+}
---
+++ spring-boot-12-actuator/src/test/java/springboot12actuator/SpringBoot12ActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot12actuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot12ActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
